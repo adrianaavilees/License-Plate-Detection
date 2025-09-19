853d26424c572a47f8559f4fed452612b2071eb9
adrianaavilees <adrianaaviles0606@gmail.com>
2025-09-19 14:19:20 +0200

    detector de matriculas best model

--- README.txt
+++ README.txt
@@ -5,5 +5,15 @@
 - En algunes imatges detecta fatal on es troba la matricula.
 TODO:
     * les matricules son regions blanques que utilitzi aixo per identificar fons clars --> VA MOLT MALAMENT AMB AQUEST AJUST image8
-    * segons la posició, on es troba del cotxe, acostumen a estar en la part inferior de davant (només pasarem fotos de la part de davant)
+    * segons la posició, on es troba del cotxe, acostumen a estar en la part inferior de davant (només pasarem fotos de la part de davant) --> no accepta variabilitat d'imatges
     * aspect ratio més petit, afectará si tenim matricules petites?
+
+19/09/2025
+- Tenemos la deteccion de la matricula hecha
+
+TODO:
+    * Segmentar los caracteres de la matricula uno por uno
+    * Transformació a grisos para quitar la parte de la E azul (COLOR_BGR2HSV)!
+    * Utilitzar otzu, fa un histograma on la imatge casi es binaria
+    * cv.findcontours en opencv
+    * Usar un adaptative threshold
--- license_plate_detector.py
+++ license_plate_detector.py
@@ -1,96 +1,90 @@
-import cv2
-from ultralytics import YOLO
-import easyocr
 import os
-import numpy as np
+import cv2
 import torch
 from pathlib import Path
+from ultralytics import YOLO
+# from google.colab.patches import cv2_imshow
 
 class LicensePlateDetector:
-    def __init__(self, model_path="license_plate_model.pt", device=None):
-        self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
+    def __init__(self, model_path="best_model.pt"):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
-        if os.path.exists(model_path):
-            self.license_model = YOLO(model_path) 
-            print(f"Loaded custom license plate model from {model_path}")
-        else:
-            self.license_model = YOLO("yolov8n.pt")
-            print("Custom model not found, using base YOLOv8n model")
-                
-        self.reader = easyocr.Reader(['en'], gpu=torch.cuda.is_available())
-        self.model_path = model_path
-    
     def create_dataset(self, dataset_path, yaml_path="dataset.yaml"):
         """Create a file dataset.yaml for training the model"""
-        
+
         config_content = f"""
-        path: {dataset_path}  
-        train: images/train  
+        path: {dataset_path}
+        train: images/train
         val: images/val
         test: images/test
-        names: 
+        names:
           0: license_plate"""
-        
+
         with open(yaml_path, 'w') as file:
             file.write(config_content)
         print(f"Dataset configuration file created at {yaml_path}")
         return yaml_path
-        
-    def train_license_model(self, dataset_path, epochs=100, batch=16):
+
+    def train_model(self, dataset_path, epochs=10, batch=8, project="license_plate_training"):
         """Train a custom model for license plate detection."""
 
         yaml_path = self.create_dataset(dataset_path)
+        
+        print("Starting training...")
+
         # Load base model
         model = YOLO("yolov8n.pt")
-
-        print("Starting training...")
         # Train the model on the custom dataset
-        results = model.train(data=yaml_path, epochs=epochs, batch=batch, project="license_plate_training", name="run1")
+        results = model.train(data=yaml_path, epochs=epochs, batch=batch, project=project, name="run1")
 
-        # Save the best model
-        best_model_path = Path("license_plate_training/run1/weights/best.pt")
-        model.save(best_model_path)
-        
+        # Route where yolo automatically saves the model
+        best_model_path = Path(project) / "run1" / "weights" / "best.pt"
+    
         print("Training completed")
 
-        return results
-    
-    def detect_license_plate(self, input_dir, output_dir):
+        return str(best_model_path)
+
+    def detect_license_plate(self, best_model_path,test_dir, output_dir, conf=0.25):
         """Detect vehicles in the image using the model trained"""
         os.makedirs(output_dir, exist_ok=True)
-        image_paths = input_dir  
-
-        for img_path in image_paths:
-            image = cv2.imread(image_path)
-            if image is None:
-                print(f"Error: Unable to load image at {image_path}")
-                return [], None
-            
-            results = self.license_model(image) #! Podemos jugar con los parametros de confianza para mejorar resultados
+        best_model = YOLO(best_model_path)
+        print(f"Detecting license plates with model {best_model_path}")
         
-            for result in results:
-                boxes = result.boxes
-                if boxes is not None:
-                    for i, box in enumerate(boxes):
-                        x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
-                        conf = box.conf[0].cpu().numpy()
-
-                        # Draw bounding box of the license plate (green)
-                        cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-                        cv2.putText(image, f"Plate {i+1}", (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)    
-        
-        out_path = os.path.join(output_dir, os.path.basename(img_path))
-        cv2.imwrite(out_path, image)
+        best_model.predict(source=test_dir, save=True, project=output_dir, name="results", conf=conf)
+        print("Detection completed")
     
-# Main
-dataset_path = r"\Repte Matriculas\BD_LicensePlate"
+    def show_results(self, output_dir):
+      """Show the results of the detection"""
+      results_dir = os.path.join(output_dir, "results")
+      saved_files = [f for f in os.listdir(results_dir) if f.lower().endswith((".jpg", ".png", ".jpeg" ))]
+      
+      if not saved_files:
+          print("No images found")
+  
+      for file_name in saved_files:
+          img_path = os.path.join(results_dir, file_name)
+          image = cv2.imread(img_path)
+          if image is not None:
+              cv2.imshow(f"Result - {file_name}", image)
+              cv2.waitKey(0)
+              cv2.destroyAllWindows()
+          else:
+              print(f"Could not read image: {img_path}")
 
-detector = LicensePlateDetector(model_path="license_plate_model.pt")
+# ================= MAIN =================
 
-# Train the model
-best_model = detector.train_license_model(dataset_path, epochs=10, batch=8)
+dataset_path = "/content/BD/BD_LicensePlate"
+test_dir = "/content/BD/BD_LicensePlate/images/test"
+output_dir = "/content/test_results"
 
-test_images_dir = os.path.join(dataset_path, "images/test")
-output_dir = r"/Repte Matricules/test_results"
-detector.detect_license_plate(test_images_dir, output_dir)
+detector = LicensePlateDetector()
 
+# 1) TRAIN and save the model. Only if we don't have the model already
+best_model_path = detector.train_model(dataset_path, epochs=10, batch=8)
+
+# 2) LOAD the model trained and detect license plates
+best_model_path = "/content/license_plate_training/best.pt"  # Ruta donde se ha guardado
+detector.detect_license_plate(best_model_path, test_dir, output_dir, conf=0.25)
+
+# 3) SHOW results
+detector.show_results(output_dir)
